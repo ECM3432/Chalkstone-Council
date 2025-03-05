628084261bdab59d626f985c77e69fe0ed5f11ce
Arkay <arkay92@outlook.com>
2025-03-05 18:50:28 +0000

    Update 'issues' urls

--- issues/urls.py
+++ issues/urls.py
@@ -1,13 +1,19 @@
 # issues/urls.py
 
-from django.urls import path
-from core import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('issues/', views.home, name='issues'),  # homepage view
+    path('', views.issue_list, name='issue_list'),
+    path('new/', views.issue_create, name='issue_create'),
+    path('<int:pk>/', views.issue_detail, name='issue_detail'),
+    path('<int:pk>/edit/', views.issue_edit, name='issue_edit'),
+    path('<int:pk>/delete/', views.issue_delete, name='issue_delete'),
+    path('category/<str:category>/', views.issue_category, name='issue_category'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

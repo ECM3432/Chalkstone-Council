944354e4afc297653f27dabfd4b4ebc6779ed010
Arkay <arkay92@outlook.com>
2025-03-05 16:20:15 +0000

    Return base.html when launching application

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
+# core/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    # Return a response to the homepage
+    return render(request, 'core/base.html')

ddd1b0257c90409ff5758846877ede50f65031bf
Arkay <arkay92@outlook.com>
2025-03-12 18:31:39 +0000

    Create urls for the analytics app

--- analytics/urls.py
+++ analytics/urls.py
@@ -5,7 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('analytics/', views.home, name='analytics'),  # homepage view
+    path('', views.analytics_home, name='analytics_home'),  # Home page
+    path('issues/', views.analytics_stats, name='analytics_stats'),  # Page 1: Overview of Issues by Status
+    path('filter/', views.analytics_filter, name='analytics_filter'),  # Page 2: Filter and Query Issues
+    path('closed-issues/', views.analytics_issues, name='analytics_issues'),  # Page 3: View Closed Issues
+    path('charts/', views.analytics_charts, name='analytics_charts'),  # Page 4: Visualize Issues in Charts
 ]
 
 if settings.DEBUG:

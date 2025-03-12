7b81ef3c619801ee5a4950be725c2ec65eefc86c
Arkay <arkay92@outlook.com>
2025-03-12 14:41:13 +0000

    Refactor issues urls

--- issues/urls.py
+++ issues/urls.py
@@ -1,7 +1,7 @@
 # issues/urls.py
 
-from django.urls import path
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.issue_list, name='issue_list'),  # Issue list page
@@ -10,4 +10,5 @@ urlpatterns = [
     path('<int:pk>/edit/', views.issue_edit, name='issue_edit'),  # Page to edit an issue
     path('<int:pk>/delete/', views.issue_delete, name='issue_delete'),  # Page to delete an issue
     path('category/<str:category>/', views.issue_category, name='issue_category'),  # Filter by category
+    path('comments/', include('comments.urls')),  # Include the comments URLs
 ]

8e0b0ac8466e16e56e205eecced04fdcccdc2440
Arkay <arkay92@outlook.com>
2025-03-12 14:39:58 +0000

    Create add_comment logic

--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,16 @@
-# comments/views.py
-from django.shortcuts import render
+from django.shortcuts import redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import CommentForm
+from issues.models import Issue
 
-# Define the 'home' view
-def home(request):
-    # You can return a simple response or render a template
-    return render(request, 'comments/base.html')  # Replace with your actual template
+@login_required
+def add_comment(request, issue_id):
+    issue = get_object_or_404(Issue, id=issue_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.issue = issue
+            comment.author = request.user
+            comment.save()
+    return redirect('issue_detail', pk=issue.id)
